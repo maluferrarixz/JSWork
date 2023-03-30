fb686f9269162a61ff5987aa148527d4c13df2ed
maluferrarixz <02609526059@senacrs.edu.br>
2023-03-30 11:50:10 -0300

    branch de atividades

---
+++ main.js
@@ -0,0 +1,93 @@
+//  ATIVIDADE ARRAY
+
+// const tarefas = [ ]
+
+// const ask1 = prompt('Quais tarefas que você precisa realizar? escreva a primeira aqui:')
+// const ask2 = prompt(' escreva a segunda aqui:')
+// const ask3 = prompt('escreva a terceria aqui:')
+
+// tarefas.push(ask1, ask2, ask3)
+
+// console.log(tarefas)
+
+// const remove = prompt("Digite o índice da tarefa que digitou anteriormente, assim poderei apagá-lo da lista!")
+// tarefas.splice(remove)
+// console.log(tarefas)
+
+//  ATIVIDADE A
+// const nome ='Maluba'
+// const idade ='17'
+// const endereço = 'bairro boa vista'
+// const profissao = 'developer'
+
+// function frase(nome,idade,endereço,profissao){
+//       console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereço} e sou ${profissao}`)
+// }console.log(frase(nome,idade,endereço,profissao))
+
+// ATIVIDADE B
+// // const A = 5
+// // const B = 3
+// // function soma(A, B){ 
+// //     const adicao = A + B
+// //     return adicao
+// // }console.log(soma(A, B))
+
+// // ATIVIDADE C
+// const A = 10
+// const B = 20
+// const maior = true
+// const menor = false
+// function condicao(A, B, maior,menor){
+//         if(A >= B){
+//         return maior
+// }else
+//         return menor
+// }console.log(condicao(A,B,maior,menor))
+
+//  ATIVIDADE D
+// // A = 9
+// // const par = true
+// // const impar = false
+// // function valores(A,par,impar){
+// //     if (A % 2 === 0){
+// //         return par
+// // }else{
+// //         return impar
+// // }}console.log(valores(A,par,impar))
+
+// ATIVIDADE E
+// // const letras = 'Maluuuuuuuuu'
+// // function palavra(letras){
+// //     console.log(letras.length) //valor de letras
+// //     console.log(letras.toUpperCase())// letras em maiusculo
+// // }console.log(palavra(letras))
+
+// CRIAR FUNÇÕES
+// // const X = prompt('Digite um valores:')
+// // const Y = prompt('Digite outro valores:')
+
+
+// // function soma(X,Y){ 
+// //     let resultado = X + Y;
+// //     return resultado;
+// // }console.log(soma(X,Y))
+
+// // function subtração(X,Y){ 
+// //     const diminui = X - Y;
+// //     return diminui;
+// // }console.log(subtração(X,Y))
+
+// // function multiplicação(X,Y){ 
+// //     const dobra = X * Y
+// //     return dobra
+// // }console.log(multiplicação(X,Y))
+
+// // function divisao(X,Y){ 
+// //     const divide = X /  Y
+// //     return divide
+// // }console.log(divisao(X,Y))
+
+
+
+
+// console.log("Teste");
